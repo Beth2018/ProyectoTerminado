a5ca2d5d5d665572dfcb98748f9110afb648b688
SUITE <user@hotmail.com>
2023-04-03 22:23:54 -0500

    first commit

---
+++ src/main/java/pe/cibertec/ecommerce/ApiCustomer/service/CustomerServiceImpl.java
@@ -0,0 +1,27 @@
+
+package pe.cibertec.ecommerce.ApiCustomer.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.cibertec.ecommerce.ApiCustomer.dao.CustomerRepositoy;
+import pe.cibertec.ecommerce.ApiCustomer.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService{
+    
+    @Autowired
+    private CustomerRepositoy customerRepository;
+            
+    @Override
+    public List<Customer> findAll() {
+        return (List<Customer>)customerRepository.findAll();
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return customerRepository.findById(id).orElse(null);
+    }
+    
+    
+}
